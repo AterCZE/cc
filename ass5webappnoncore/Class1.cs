769ec1ed40a07181d14964cfa1fe484e3a1ae118
hous01 <hous01@vse.cz>
2020-12-26 20:20:24 +0100

    Initial commit

---
+++ ass5webappnoncore/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ass5webappdesk
+{
+    public class fillTable
+    {
+        public static string getWhileLoopData()
+        {
+            string connectionString = "Server=tcp:ass5bilgi.database.windows.net,1433;Initial Catalog=dbass5bilgi;Persist Security Info=False;User ID=KristynAdmin;Password=w$^9ChL3Kqze@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            string htmlStr = "";
+            SqlConnection thisConnection = new SqlConnection(connectionString);
+            SqlCommand thisCommand = thisConnection.CreateCommand();
+            thisCommand.CommandText = "select top 10 CustomerID, Title, FirstName, LastName, EmailAddress from [SalesLT].[Customer]";
+            thisConnection.Open();
+            SqlDataReader reader = thisCommand.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int ID = reader.GetInt32(0);
+                string Title = reader.GetString(1);
+                string FirstName = reader.GetString(2);
+                string LastName = reader.GetString(3);
+                string EmailAddress = reader.GetString(4);
+
+                htmlStr += "<tr><td>" + ID + "</td><td>" + Title + "</td><td>" + FirstName + "</td><td>" + LastName + "</td><td>" + EmailAddress + "</td></tr>";
+            }
+
+            thisConnection.Close();
+            return htmlStr;
+        }
+    }
+}
---
+++ ass5webappnoncore/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ass5webappnoncore._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+ <div class="text-center">
+        <h1 class="display-4">Assignment 5</h1>
+        <p>Student ID: 12001018</p>
+    </div>
+
+
+
+      <table width="100%" align="center" cellpadding="2" cellspacing="2" border="0" bgcolor="#EAEAEA">
+            <tr align="left" style="background-color:#004080;color:White;">
+                <td>ID</td>
+                <td>Title</td>
+                <td>FirstName</td>
+                <td>LasttName</td>
+                <td>EmailAddress</td>
+            </tr>
+
+
+            <%Response.Write(ass5webappdesk.fillTable.getWhileLoopData());%>
+
+        </table>
+    </asp:Content>
+
